8f4c61248e0a9bf185cdd615eec220f72238e6fc
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2019-01-30 14:07:52 -0500

    add unit test for condition value assignment

---
+++ src/groovy/test/unit/ConditionUnitTests.groovy
@@ -0,0 +1,29 @@
+/**
+ * Tests field operations
+ */
+
+import gov.noaa.messageapi.factories.SessionFactory;
+import gov.noaa.messageapi.interfaces.ISession;
+import gov.noaa.messageapi.interfaces.IRequest;
+import gov.noaa.messageapi.interfaces.IRecord;
+import gov.noaa.messageapi.interfaces.ICondition;
+
+class ConditionUnitTests extends spock.lang.Specification {
+
+
+    def "Tests giving a value to a condition."() {
+        given:
+            def conditionValue = "testConditionValue"
+            def sessionSpec = this.getClass().getResource('sessions/sqlite-jdbc-clisam.json').getPath()
+            ISession session = SessionFactory.create(sessionSpec)
+            IRequest request = session.createAddRequest()
+            IRecord record = request.createRecord()
+            ICondition condition = record.getCondition("1")
+        when:
+            condition.setValue(conditionValue)
+        then:
+            condition.getValue() == conditionValue
+    }
+
+
+}
