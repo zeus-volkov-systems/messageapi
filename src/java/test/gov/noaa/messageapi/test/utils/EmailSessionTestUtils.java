eeee25681714ceff07b6c5fe37a53b6bac3d52da
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2019-02-03 13:54:06 -0500

    Add new testRequest record/condition set for EmailSession testing

--- src/java/test/gov/noaa/messageapi/test/utils/EmailSessionTestUtils.java
+++ src/java/test/gov/noaa/messageapi/test/utils/EmailSessionTestUtils.java
@@ -84,5 +84,26 @@ public class EmailSessionTestUtils {
         return request;
     }
 
+    public static IRequest getTestAddRequest5() throws Exception {
+        IRequest request = getAddRequest();
+        IRecord record1 = request.createRecord();
+        IRecord record2 = request.createRecord();
+        IRecord record3 = request.createRecord();
+        IRecord record4 = request.createRecord();
+        IRecord record5 = request.createRecord();
+        setTestRecordFields(record1,"sender1", "recipient1", "subject1", "body1");
+        setTestRecordFields(record2, "sender2", "recipient2", "subject2", null); //should be rejected due to missing required field 'body'
+        setTestRecordFields(record3,"sender3", "recipient3", "subject3", "body3");
+        setTestRecordFields(record4, "sender4", "recipient4", "subject4", "body4");
+        setTestRecordFields(record5, "sender5", "recipient5", "subject5", "body5");
+        setTestRecordConditions(record1, "sender2"); //record 1 must have a sender of sender2 (fail)
+        setTestRecordConditions(record2, "sender5"); //record 2 must have a sender of sender5 (but this record was already removed) (fail)
+        setTestRecordConditions(record3, "sender3"); //record 3 must have a sender of sender3 (success)
+        setTestRecordConditions(record4, "sender4"); //record 4 must have a sender of sender4 (success)
+        //we don't add a condition for record5 (success) -> this is currently failing. we should be removing the condition for 5 because it wasn't valued
+        // should end up with 3 records - 3, 4, 5
+        return request;
+    }
+
 
 }
