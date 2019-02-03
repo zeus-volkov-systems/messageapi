da3e54fe065524216413dddb858dafa0e929c7ff
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2019-02-03 14:00:28 -0500

    added successful large-unit test for testing entire 'prepare' method for add request

--- src/groovy/test/unit/RequestUnitTests.groovy
+++ src/groovy/test/unit/RequestUnitTests.groovy
@@ -2,9 +2,12 @@
  * Tests request operations
  */
 
+import gov.noaa.messageapi.test.utils.EmailSessionTestUtils;
+
 import gov.noaa.messageapi.factories.SessionFactory;
 import gov.noaa.messageapi.interfaces.ISession;
 import gov.noaa.messageapi.interfaces.IRequest;
+import gov.noaa.messageapi.interfaces.IRejection;
 import gov.noaa.messageapi.interfaces.IRecord;
 import gov.noaa.messageapi.interfaces.IField;
 
@@ -31,5 +34,21 @@ class RequestUnitTests extends spock.lang.Specification {
         then: "Then the two requests should be different instances."
             r1 != r2
     }
+
+    def "Test preparation of an add request and generation of schema rejections."() {
+        given: "A standard email session test setup with add request and the first record in that request"
+            IRequest testRequest = EmailSessionTestUtils.getTestAddRequest5()
+        when: "We call prepare on the test request"
+            List<IRejection> rejections = testRequest.prepare()
+        then: "We should have the proper number of rejections due to the overall running process"
+            rejections.size() == 2
+            IRejection rej1 = rejections.get(0)
+            IRejection rej2 = rejections.get(1)
+            rej1.getReasons() == ["Required field body was missing a value."]
+            rej1.getRecord().getField("sender").getValue() == "sender2"
+            rej2.getReasons() == ["The record fields were invalidated against specified conditions."]
+            rej2.getRecord().getField("sender").getValue() == "sender1"
+        }
+
 
 }
