fd32be22fe5a0cefdddd91c36a05af9e7916b2ae
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2019-02-15 14:40:35 -0500

    add method to bin schema records into ContainerRecord fieldsets

--- src/java/main/gov/noaa/messageapi/utils/containers/FieldSetUtils.java
+++ src/java/main/gov/noaa/messageapi/utils/containers/FieldSetUtils.java
@@ -17,7 +17,22 @@ public class FieldSetUtils {
         }).collect(Collectors.toList());
     }
 
-    public static IContainerRecord setFieldValues(IContainerRecord containerRecord, List<IField> fields) {
+    /**
+     * Updates container record field set values with any schema field values with a matching field.
+     * @param  containerRecord The container record containing field sets with empty fields
+     * @param  schemaFields    A flat set of fields that need to be binned
+     * @return                 The updated container record with all values set
+     */
+    public static IContainerRecord setFieldValues(IContainerRecord containerRecord, List<IField> schemaFields) {
+        schemaFields.stream().forEach(schemaField -> {
+            containerRecord.getFieldSets().stream().forEach(fieldSet -> {
+                fieldSet.getFields().stream().forEach(fieldSetField -> {
+                    if (schemaField.getName().equals(fieldSetField.getName())) {
+                        fieldSetField.setValue(schemaField.getValue());
+                    }
+                });
+            });
+        });
         return containerRecord;
     }
 
