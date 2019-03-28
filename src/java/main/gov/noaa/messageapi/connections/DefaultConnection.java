8f7c6badc031590b1c1b2aa618d44f6f375402da
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2019-03-28 14:46:50 -0400

    implement classifiers access (setting/getting) in protocol Connection class (DefaultConnection).

--- src/java/main/gov/noaa/messageapi/connections/DefaultConnection.java
+++ src/java/main/gov/noaa/messageapi/connections/DefaultConnection.java
@@ -1,8 +1,9 @@
 package gov.noaa.messageapi.connections;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 import gov.noaa.messageapi.interfaces.IConnection;
 import gov.noaa.messageapi.interfaces.IProtocolRecord;
@@ -12,13 +13,28 @@ import gov.noaa.messageapi.interfaces.IContainerRecord;
 public class DefaultConnection extends BaseConnection implements IConnection {
 
     private String id;
-    private List<String> bins;
+    private List<String> bins = null;
+    private Map<String,List<String>> classifiers = null;
 
     @SuppressWarnings("unchecked")
     public DefaultConnection(String endpointClass, Map<String,Object> connectionMap) throws Exception {
         super(endpointClass, (Map<String,Object>) connectionMap.get("parameters"));
-        setId((String) connectionMap.get("id"));
-        setBins(connectionMap.get("bins"));
+        try {
+            setId((String) connectionMap.get("id"));
+            if (connectionMap.containsKey("bins")) {
+                setBins(connectionMap.get("bins"));
+            } else {
+                setBins(new ArrayList<String>());
+            }
+            if (connectionMap.containsKey("classifiers")) {
+                setClassifiers((Map<String,List<String>>)connectionMap.get("classifiers"));
+            } else {
+                setClassifiers(new HashMap<String,List<String>>());
+            }
+        } catch (Exception e) {
+            System.out.println("Error in connection instantiation. Stacktrace to follow.");
+            System.out.println(e.getStackTrace());
+        }
     }
 
     public String getId() {
@@ -27,6 +43,24 @@ public class DefaultConnection extends BaseConnection implements IConnection {
 
     public List<String> getBins() {
         return this.bins;
+    }
+
+    /**
+     * Returns the list of classifiers that have been associated with this connection.
+     * @return an arraylist containing every classifier associated with this connection.
+     */
+    public List<String> getClassifiers() {
+        return new ArrayList<>(this.classifiers.keySet());
+    }
+
+    /**
+     * Returns all the classifier values for a specific classifier key
+     * @param  classifierKey The particular classifier to return values for
+     *                      (e.g., classifiers= [namespace], and ["namespace"] has "namespace1, namespace2")
+     * @return               Outputs a list of classifiers for the provided key.
+     */
+    public List<String> getClassifers(String classifierKey) {
+        return this.classifiers.get(classifierKey);
     }
 
     public IProtocolRecord process(List<IContainerRecord> containerRecords) {
@@ -45,5 +79,9 @@ public class DefaultConnection extends BaseConnection implements IConnection {
         } else if (bins instanceof List) {
             this.bins = (List<String>) bins;
         }
+    }
+
+    private void setClassifiers(Map<String,List<String>> classifiers) {
+        this.classifiers = classifiers;
     }
 }
