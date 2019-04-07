a0e5b04a9906ec42fbef65dc73376c3bdae69dd3
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2019-04-07 09:23:42 -0400

    add docstring to mergeMapList method

--- src/java/main/gov/noaa/messageapi/utils/general/MapUtils.java
+++ src/java/main/gov/noaa/messageapi/utils/general/MapUtils.java
@@ -6,6 +6,13 @@ import java.util.List;
 
 public class MapUtils {
 
+    /**
+     * Merges a list of maps of arbitrary K/V type into a new single map that contains
+     * all keys/values from each map in turn. this method uses putAll and traverses the list
+     * in order, so new values will overwrite old (if the key already existed on the new map).
+     * @param  list A list of maps to merge
+     * @return      A new map with all keys from all maps merged
+     */
     public static <K,V> Map<K,V> mergeMapList(List<Map<K,V>> list) {
         Map<K,V> mergedMap = new HashMap<K,V>();
         list.stream().forEach(item -> {
