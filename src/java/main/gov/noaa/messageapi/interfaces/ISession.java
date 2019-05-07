fef5a49987ee2dafde9ec3ea0eff7a863b382215
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2019-05-07 10:37:46 -0400

    modify Session interface to allow access to container, schema, and protocol components

--- src/java/main/gov/noaa/messageapi/interfaces/ISession.java
+++ src/java/main/gov/noaa/messageapi/interfaces/ISession.java
@@ -5,4 +5,7 @@ import gov.noaa.messageapi.interfaces.IRequest;
 public interface ISession {
 
     public IRequest createRequest();
+    public IContainer getContainer();
+    public IProtocol getProtocol();
+    public ISchema getSchema();
 }
