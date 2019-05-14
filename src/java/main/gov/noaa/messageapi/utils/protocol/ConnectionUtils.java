822b8c588be3a241f76681512a860e9e56342eb4
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2019-05-14 14:31:31 -0400

    add static getConnectionTransformations method in ConnectionUtils for assembling complete list of transformation ids (including recursively referenced) for a given connection.

--- src/java/main/gov/noaa/messageapi/utils/protocol/ConnectionUtils.java
+++ src/java/main/gov/noaa/messageapi/utils/protocol/ConnectionUtils.java
@@ -1,10 +1,16 @@
 package gov.noaa.messageapi.utils.protocol;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 import gov.noaa.messageapi.interfaces.IConnection;
 import gov.noaa.messageapi.interfaces.IProtocolRecord;
 import gov.noaa.messageapi.interfaces.IPacket;
+import gov.noaa.messageapi.utils.general.ListUtils;
 
 public class ConnectionUtils {
 
@@ -15,6 +21,38 @@ public class ConnectionUtils {
             });
         }
         return null;
+    }
+
+    /**
+     * [getConnectionTransformations description]
+     * @param  namedTransformations  [description]
+     * @param  rawTransformationMaps [description]
+     * @return                       [description]
+     */
+    public static List<String> getConnectionTransformations(List<String> namedTransformations, List<Map<String,Object>> rawTransformationMaps) {
+        List<String> connTransformations = new ArrayList<String>();
+        return ListUtils.removeAllNulls(ListUtils.flatten(namedTransformations.stream().map(namedTransformation -> {
+            return getFullConnectionList(namedTransformation, connTransformations, rawTransformationMaps);
+        }).collect(Collectors.toList())));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static List<String> getFullConnectionList(String candidate, List<String> confirmations, List<Map<String,Object>> sourceMaps) {
+        Optional<Map<String,Object>> candidateMap = sourceMaps.stream().filter(sourceMap -> ((String) sourceMap.get("id")).equals(candidate)).findAny();
+        if (candidateMap.isPresent()) {
+            confirmations.add((String) candidateMap.get().get("id"));
+            if (candidateMap.get().get("records") instanceof Map) {
+                ((Map<String, Object>) candidateMap.get().get("records")).values().stream().map(candidateRecord -> {
+                    if (candidateRecord instanceof Map) {
+                        if (((Map<String,Object>)candidateRecord).containsKey("TRANSFORMATION")) {
+                            return getFullConnectionList((String)((Map<String,Object>)candidateRecord).get("TRANSFORMATION"), confirmations, sourceMaps);
+                        }
+                    }
+                    return confirmations;
+                }).collect(Collectors.toList());
+            }
+        }
+        return confirmations;
     }
 
 }
