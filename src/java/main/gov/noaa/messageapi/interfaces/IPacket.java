f94fd1d95c08b80a7ec4b7f2b7a9932598363e30
Ryan Berkheimer <ryan.berkheimer@noaa.gov>
2019-06-23 18:59:34 -0400

    Added docstrings to IPacket and ISession.

--- src/java/main/gov/noaa/messageapi/interfaces/IPacket.java
+++ src/java/main/gov/noaa/messageapi/interfaces/IPacket.java
@@ -6,18 +6,55 @@ import gov.noaa.messageapi.interfaces.IRecord;
 import gov.noaa.messageapi.interfaces.IRejection;
 
 /**
+ * IPackets are simple wrapper containers that hold together
+ * a list of records and a list of rejections, and provide
+ * methods for setting, retrieving, and adding to these
+ * contained collections. IPackets are the return type of
+ * Endpoints.
  * @author Ryan Berkheimer
  */
 public interface IPacket {
 
+    /**
+     * Sets the records for the IPacket to the passed list of Records.
+     * This replaces anything that was there before.
+     */
     public void setRecords(List<IRecord> records);
+
+    /**
+     * Adds a record to the existing list of records in the IPacket.
+     */
     public void addRecord(IRecord record);
+
+    /**
+     * Adds all the passed records to the existing list of records in the IPacket.
+     */
     public void addRecords(List<IRecord> records);
+
+    /**
+     * Returns a list of all current records held by the IPacket.
+     */
     public List<IRecord> getRecords();
 
+    /**
+     * Sets the rejections for the IPacket to the passed list of Rejections.
+     * This replaces anything that was there before.
+     */
     public void setRejections(List<IRejection> rejections);
+
+    /**
+     * Adds the given rejection to the existing list of IPacket rejections.
+     */
     public void addRejection(IRejection rejection);
+
+    /**
+     * Adds all given rejections to the existing list of IPacket rejections.
+     */
     public void addRejections(List<IRejection> rejections);
+
+    /**
+     * Returns a list of all current rejections held by the IPacket.
+     */
     public List<IRejection> getRejections();
 
 }
--- src/java/main/gov/noaa/messageapi/interfaces/ISession.java
+++ src/java/main/gov/noaa/messageapi/interfaces/ISession.java
@@ -3,12 +3,35 @@ package gov.noaa.messageapi.interfaces;
 import gov.noaa.messageapi.interfaces.IRequest;
 
 /**
+ * ISession is the primary container of MessageAPI.
+ * An ISession holds definitions for its three dimensions
+ * (Schema, Container, and Protocol), and provides a mechanism
+ * for creating requests based on these definitions.
+ * Sessions are primarily used as RequestFactories. Typically,
+ * ISessions in MessageAPI are created using JSON manifest files.
  * @author Ryan Berkheimer
  */
 public interface ISession {
 
+    /**
+     * Creates a new Request derived from the Session. This Request
+     * is created using the Schema, Container, and Protocol layers
+     * defined by this Session.
+     */
     public IRequest createRequest();
+
+    /**
+     * Returns the Container held by this Session.
+     */
     public IContainer getContainer();
+
+    /**
+     * Returns the Protocol held by this Session.
+     */
     public IProtocol getProtocol();
+
+    /**
+     * Returns the Schema held by this Session.
+     */
     public ISchema getSchema();
 }
